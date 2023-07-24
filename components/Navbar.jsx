2c2432286cfcb095b415e12550d82d6c1e20d393
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-07-25 05:13:18 +0700

    Fixed some styling to Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Link from "next/link";
-import { RiAddBoxLine } from "react-icons/ri";
+import { RiAddBoxLine, RiAddCircleLine } from "react-icons/ri";
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center bg-blue-400 px-8 py-3">
-      <Link className="text-white font-bold text-xl" href={"/"}>
+      <Link className="text-white font-bold text-2xl pl-4" href={"/"}>
         ToDecode-App
       </Link>
       <Link className="text-white text-bold p-2" href={"/addTask"}>
-        <RiAddBoxLine size={50} />
+        <RiAddBoxLine size={60} />
+        {/* <RiAddCircleLine size={60} /> */}
       </Link>
     </nav>
   );
